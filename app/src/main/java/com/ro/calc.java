64236a229da4c8ed72a2c1133d70437b1147752f
Jagrav bhatt <jagravbhatt01@gmail.com>
2026-01-30 19:57:18 +0530

    updated

--- app/src/main/java/com/ro/calc.java
+++ app/src/main/java/com/ro/calc.java
@@ -8,20 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class signup extends AppCompatActivity {
+public class calc extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_calc);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-//        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-//        startActivity(intent);
-
     }
 }
