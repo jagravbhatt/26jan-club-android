3cf18ac195cdde4b833e07d360a89ed469f5ca32
Jagrav Bhatt <your_email@gmail.com>
2026-02-11 18:37:03 +0530

    updated

--- app/src/main/java/com/ro/LoginActivity.java
+++ app/src/main/java/com/ro/LoginActivity.java
@@ -2,16 +2,12 @@ package com.ro;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText edtEmail, edtPassword;
     private Button btnLogin;
 
     @Override
@@ -19,43 +15,11 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // Initialize UI components
-        edtEmail = findViewById(R.id.edtemail);
-        edtPassword = findViewById(R.id.edtpassword);
         btnLogin = findViewById(R.id.btnlogin);
 
-        // Button click
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        btnLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(signup.this, MainActivity.class);
+            startActivity(intent);
 
-                String email = edtEmail.getText().toString().trim();
-                String password = edtPassword.getText().toString().trim();
-
-                if (email.isEmpty() || password.isEmpty()) {
-                    Toast.makeText(LoginActivity.this,
-                            "Please enter email and password",
-                            Toast.LENGTH_SHORT).show();
-                } else {
-                    // Hardcoded login check
-                    if (email.equals("test@example.com") && password.equals("1234")) {
-
-                        Toast.makeText(LoginActivity.this,
-                                "Login successful",
-                                Toast.LENGTH_SHORT).show();
-
-                        // GO TO SIGNUP / HOME PAGE
-                        Intent intent = new Intent(LoginActivity.this.acitivity_Signup.class);
-                        startActivity(intent);
-                        finish();
-
-                    } else {
-                        Toast.makeText(LoginActivity.this,
-                                "Invalid email or password",
-                                Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }
-        });
-    }
+        }
 }
--- app/src/main/java/com/ro/signup.java
+++ app/src/main/java/com/ro/signup.java
@@ -2,30 +2,23 @@ package com.ro;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-
-
 
 public class signup extends AppCompatActivity {
+
+//    Button btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_signup);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-       Intent intent = new Intent(MainActivity.this, forgotpassword.class);
-       startActivity(intent);
 
+//        btnNext = findViewById(R.id.btnsignup);
+//
+//        btnNext.setOnClickListener(v -> {
+           Intent intent = new Intent(signup.this, MainActivity.class);
+           startActivity(intent);
     }
 }
